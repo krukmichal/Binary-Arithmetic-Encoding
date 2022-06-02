ebf4c51854761b5783387764d9a9baa9594bb62e
ilyashich <ilyashich99@gmail.com>
2022-06-02 14:26:34 +0200

    latest version

--- arithmetic_encoding.py
+++ arithmetic_encoding.py
@@ -1,24 +1,45 @@
 import time
 from bitarray import bitarray
-from bitarray.util import base2ba
 from math import floor
 
-def zero_out(array,length): # zero out the array from 0 to 256
+
+def calc_prob(word):
+    word = "".join(sorted(word))
+    dct = dict()
+    total_count = len(word)
+
+    for num in word:
+        dct[num] = dct.get(num, 0) + 1
+
+    res = dict()
+    dct_sum = 0
+    for key in dct:
+        dct[key] = dct[key] / total_count
+        res[key] = (dct_sum, dct[key] + dct_sum)
+        dct_sum += dct[key]
+
+    return res
+
+
+def zero_out(array, length):  # zero out the array from 0 to 256
     for i in range(length):  # zero out count array
-        array.insert(i,0)
+        array.insert(i, 0)
     return array
 
+
 # Populates the Cum_Count Array
-def cdf(count,cum_count): # populate cum_count array
-    for i in range(1,257):
-        cum_count[i] += count[i-1]
-        cum_count[i] += cum_count[i-1]
+def cdf(count, cum_count):  # populate cum_count array
+    for i in range(1, 257):
+        cum_count[i] += count[i - 1]
+        cum_count[i] += cum_count[i - 1]
     return cum_count
 
-def fill_count(count,seq):
-    for byte in seq: # fill count
+
+def fill_count(count, seq):
+    for byte in seq:  # fill count
         count[byte] += 1
     return count
+
 
 def fill_total_count(seq):
     total_count = 0
@@ -26,25 +47,24 @@ def fill_total_count(seq):
         total_count += 1
     return total_count
 
+
 def flipBit(string):
-    bits = int(string,base=2)
-    bits = bin(int(string,base=2))[2:].zfill(8)
+    bits = int(string, base=2)
+    bits = bin(int(string, base=2))[2:].zfill(8)
     bitz = bits[0]
     if bitz == '0':
-        bits = '1'+string[1:]
+        bits = '1' + string[1:]
     if bitz == '1':
-        bits = '0'+string[1:]
+        bits = '0' + string[1:]
     return bits
 
-def giveCoB(string):
-    bits = int(string,base=2)
-    bits = bin(int(string,base=2))[2:].zfill(8)
-    bitz = bits[0]
-    if bitz == '0':
-        bits = '1'
-    if bitz == '1':
-        bits = '0'
-    return bits
+
+def giveCoB(bit):
+    if bit == '0':
+        return '1'
+    if bit == '1':
+        return '0'
+
 
 # def count_decode(cum_count):
 #     count=info
@@ -58,80 +78,91 @@ def binary(num):
     byte = bin(num)[2:].zfill(8)
     return byte
 
+
 def calculate_cum_count(seq):
-    count = [] 
-    cum_count = [] 
-    count = zero_out(count,256) 
-    count = fill_count(count,seq)
-    cum_count = zero_out(cum_count, 257) 
-    cum_count = cdf(count, cum_count) 
+    count = []
+    cum_count = []
+    count = zero_out(count, 256)
+    count = fill_count(count, seq)
+    cum_count = zero_out(cum_count, 257)
+    cum_count = cdf(count, cum_count)
     return cum_count
 
 
-def encode(seq, cum_count):
-
+def encode(seq, prob_table):
     start_time = time.time()
     print()
     print("Compression start...")
     # count = [] 
     # cum_count = [] 
-    outbytes = [] 
-    scale3 = 0 
-    lower = 0 
-    upper = 255 
+    outbytes = []
+    scale3 = 0
+    lower = 0
+    upper = 255
 
     # count = zero_out(count,256) 
     # count,total_count = fill(count,seq)
     # cum_count = zero_out(cum_count, 257) 
     # cum_count = cdf(count, cum_count) 
 
-    total_count = fill_total_count(seq)
+    # total_count = fill_total_count(seq)
 
-    print("\t Initial word : ", seq)
-    print("\t Input size: ", total_count*8, " bits")
+    # print("\t Initial word : ", seq)
+    print("\t Input size: ", len(seq) * 8, " bits")
 
-    for byte in seq: 
-        bin_low = bin(lower)[2:].zfill(8)
-        bin_up = bin(upper)[2:].zfill(8)
+    for byte in seq:
+        bin_low = binary(lower)
+        bin_up = binary(upper)
 
         lower_old = lower
         upper_old = upper
-        lower = floor(lower_old + ((upper_old - lower_old  +1) * cum_count[byte]) / total_count) 
-        upper = floor(lower_old + (((upper_old - lower_old + 1) * cum_count[byte + 1]) / total_count) - 1)
+        lower = floor(lower_old + ((upper_old - lower_old + 1) * prob_table[byte][0]))
+        upper = floor(lower_old + ((upper_old - lower_old + 1) * prob_table[byte][1]) - 1)
 
-        bin_low = bin(lower)[2:].zfill(8)
-        bin_up = bin(upper)[2:].zfill(8)
+        bin_low = binary(lower)
+        bin_up = binary(upper)
 
-        while ( (bin_low[0] == bin_up[0]) or ((bin_low[1] == '1') and (bin_up[1] == '0')) ):
-            bin_low = bin(lower)[2:].zfill(8)
-            bin_up = bin(upper)[2:].zfill(8)
+        while (bin_low[0] == bin_up[0]) or ((bin_low[1] == '1') and (bin_up[1] == '0')):
+            # bin_low = binary(lower)
+            # bin_up = binary(upper)
 
             if bin_low[0] == bin_up[0]:
-                outbytes.append(bin_low[0]) 
-                lower = int(bin_low[1:8] + '0', base = 2) 
-                upper = int(bin_up[1:8] + '1', base = 2) 
+                outbytes.append(bin_low[0])
+                lower = int(bin_low[1:8] + '0', base=2)
+                upper = int(bin_up[1:8] + '1', base=2)
+                bin_low = binary(lower)
+                bin_up = binary(upper)
 
                 while scale3 > 0:
-                    outbytes.append(giveCoB(bin_low))
+                    outbytes.append(giveCoB(bin_low[0]))
                     scale3 += -1
 
             else:
-                if ((bin_low[1] == '1') and (bin_up[1] == '0') ): 
-                    lower = int(bin_low[0] + bin_low[2:8] + '0', base = 2) 
-                    upper = int(bin_up[0] + bin_up[2:8] + '1', base = 2) 
-                    bin_low = bin(lower)[2:].zfill(8)
-                    bin_up = bin(upper)[2:].zfill(8)
+                if (bin_low[1] == '1') and (bin_up[1] == '0'):
+                    lower = int(bin_low[0] + bin_low[2:8] + '0', base=2)
+                    upper = int(bin_up[0] + bin_up[2:8] + '1', base=2)
+                    bin_low = binary(lower)
+                    bin_up = binary(upper)
                     scale3 += 1
 
-    outbytes.append(bin_low[0])
+    i = 0
+    while bin_low[i] != '0' and i < 8:
+        outbytes.append(bin_low[i])
+        i += 1
 
-    while scale3 > 0:
-        outbytes.append('1')
-        scale3 += -1
-
-    outbytes.append(bin_low[1:8])
+    # while scale3 > 0:
+    #     outbytes.append('1')
+    #     scale3 += -1
+    #
+    # outbytes.append(bin_low[1:8])
 
     out = "".join(outbytes)
+
+    added_bits = 8 - len(out) % 8
+    for _ in range(added_bits):
+        out = '0' + out
+
+
 
     output = bitarray(out)
     outbytes = out
@@ -139,16 +170,16 @@ def encode(seq, cum_count):
 
     end_time = time.time()
     print("\t Output size : ", len(out), " bits")
-    print("\t Compression ratio : {:.1f}".format(total_count*8/len(out)), " input bytes per output byte" )
-    print("\t Time elapsed {:.5f}".format((end_time-start_time)), "secomds")
+    print("\t Compression ratio : {:.1f}".format(len(seq) * 8 / len(out)), " input bytes per output byte")
+    print("\t Time elapsed {:.5f}".format((end_time - start_time)), "secomds")
     print("End of compression...")
     print()
+    print("Encoded: ", out)
 
-    
-    return outstuff
+    return outstuff, scale3, added_bits
 
 
-def decode(seq, info):
+def decode(seq, prob_table, scale3):
     """
     Args:
         seq (Bytes): Byte sequence to be decompressed.
@@ -162,77 +193,96 @@ def decode(seq, info):
 
     print()
     print("Decompression start...")
-    
-    cum_count = info # cum_count:cdf array.
-    message = "" # outbytes:output sequnce of bytes
-    lower = 0 # lower:lower bound
-    upper = 255 # upper bound
-    total_count = info[len(info)-1]
-    m = 8 # number of bits in buffer
-    offset=0
-    output = bytearray()
-    
-    k = 0
-    
+
+    # cum_count = info  # cum_count:cdf array.
+    message = ""  # outbytes:output sequnce of bytes
+    lower = 0  # lower:lower bound
+    upper = 255  # upper bound
+    # total_count = info[len(info) - 1]
+    decoded_count = 1
+    m = 8  # number of bits in buffer
+    offset = 0
+    output = []
+
+    # k = 0
+
     for byte in seq:
         message = message + binary(byte)
-    
-    for _ in range(total_count):
-        
-        t_bin = message[offset:offset+8] # **(read first m bits of received bitstream into tag t)**
-        t = int(t_bin,base=2)
-        x = ( (((t - lower + 1) * total_count) - 1 ) / (upper - lower + 1))
-        
-        for j in range(256):
-            if x >= cum_count[j]:
-                varx = 0
-            else:
-                output.append(j-1)
+
+    while offset + m <= len(message):
+
+        t_bin = message[offset:offset + m]  # **(read first m bits of received bitstream into tag t)**
+        # print(t_bin)
+        t = int(t_bin, base=2)
+        x = (t - lower) / (upper - lower + 1)
+
+        for prob_range in prob_table:
+            if prob_table[prob_range][0] <= x < prob_table[prob_range][1]:
+                output.append(prob_range)
                 lower_old = lower
                 upper_old = upper
-                lower = floor(lower_old + (( upper_old - lower_old + 1 ) * cum_count[j-1]) / total_count)
-                upper = floor(lower_old + ((( upper_old - lower_old + 1 ) * cum_count[j]) / total_count) - 1)
+                lower = floor(lower_old + ((upper_old - lower_old + 1) * prob_table[prob_range][0]))
+                upper = floor(lower_old + ((upper_old - lower_old + 1) * prob_table[prob_range][1]) - 1)
+
                 bin_low = binary(lower)
                 bin_up = binary(upper)
-                
-                # COMPARE MSB OF LOWER AND UPPER
-                while(True):
+
+                while (bin_low[0] == bin_up[0]) or ((bin_low[1] == '1') and (bin_up[1] == '0')):
+
                     if bin_low[0] == bin_up[0]:
-                        
-                        #shift out msb
-                        lower = int(bin_low[1:8] + '0',base = 2)
-                        upper = int(bin_up[1:8] + '1', base = 2)
+                        lower = int(bin_low[1:8] + '0', base=2)
+                        upper = int(bin_up[1:8] + '1', base=2)
                         bin_low = binary(lower)
                         bin_up = binary(upper)
-                        
                         offset += 1
+
                     else:
-                        break
-                        
-                #check E3
-                while(True):
-                    bin_low = binary(lower)
-                    bin_up = binary(upper)
-                    
-                    if ( (bin_low[1] == '1') and (bin_up[1] == '0')):
-                       # print("**SHIFT E3**")
-                        lower = int(bin_low[0] + bin_low[2:8] + '0', base = 2)
-                        upper = int(bin_up[0] + bin_up[2:8] + '1', base = 2)
-                        bin_low = binary(lower)
-                        bin_up = binary(upper)
-                        message = message[:offset] + str(1 - int(message[offset+1])) + message[offset+2:]
-                        # tag = message[offset:offset+m]
-                        # int_tag = int(tag,base = 2)   
-                    else:
-                        break
-                break
-            
-    output = bytes(output)
+                        if (bin_low[1] == '1') and (bin_up[1] == '0'):
+                            for _ in range(scale3):
+                                lower = int(bin_low[0] + bin_low[1] + bin_low[3:8] + '0', base=2)
+                                upper = int(bin_up[0] + bin_up[1] + bin_up[3:8] + '1', base=2)
+                                offset += 1
+                                bin_low = binary(lower)
+                                bin_up = binary(upper)
+
+                # # COMPARE MSB OF LOWER AND UPPER
+                # while True:
+                #     if bin_low[0] == bin_up[0]:
+                #
+                #         # shift out msb
+                #         lower = int(bin_low[1:8] + '0', base=2)
+                #         upper = int(bin_up[1:8] + '1', base=2)
+                #         bin_low = binary(lower)
+                #         bin_up = binary(upper)
+                #
+                #         offset += 1
+                #     else:
+                #         break
+                #
+                # # check E3
+                # while True:
+                #     bin_low = binary(lower)
+                #     bin_up = binary(upper)
+                #
+                #     if (bin_low[1] == '1') and (bin_up[1] == '0'):
+                #         # print("**SHIFT E3**")
+                #         lower = int(bin_low[0] + bin_low[2:8] + '0', base=2)
+                #         upper = int(bin_up[0] + bin_up[2:8] + '1', base=2)
+                #         bin_low = binary(lower)
+                #         bin_up = binary(upper)
+                #         message = message[:offset] + str(1 - int(message[offset + 1])) + message[offset + 2:]
+                #         # tag = message[offset:offset+m]
+                #         # int_tag = int(tag,base = 2)
+                #     else:
+                #         break
+                # break
+
+    #output = bytes(output)
 
     end_time = time.time()
-    print("\t Input size : ",len(seq)*8, " bits")
-    print("\t Output size : ", len(output)*8, " bits")
-    print("\t Time elapsed {:.5f}".format((end_time-start_time)), "seconds")
+    print("\t Input size : ", len(seq) * 8, " bits")
+    print("\t Output size : ", len(output) * 8, " bits")
+    print("\t Time elapsed {:.5f}".format((end_time - start_time)), "seconds")
     print("End of decompression")
     print()
     return output
--- test.py
+++ test.py
@@ -1,8 +1,44 @@
 from arithmetic_encoding import *
+from bitarray import *
+import matplotlib.pyplot as plt
+
+
+def get_bytes_from_file(filename):
+    with open(filename, 'rb') as pgmf:
+        return plt.imread(pgmf)
+
 
 if __name__ == "__main__":
-    word = b"adfgjoahrgb"
-    cum_count = calculate_cum_count(word)
-    encoded = encode(b"adfgjoahrgb", cum_count)
-    decoded = decode(encoded, cum_count)
-    print(decoded)
+    #mybytearray = get_bytes_from_file("./test_data/geometr_05.pgm")
+    # word = get_bytes_from_file("./test_data/uniform.pgm")
+    # new_word = []
+    # for a in word:
+    #     for b in a:
+    #         new_word.append(b)
+    #
+    # sorted_word = new_word.copy()
+    # sorted_word.sort()
+    # prob = calc_prob(sorted_word)
+    # cum_count = calculate_cum_count(new_word)
+    # print(cum_count)
+    # encoded = encode(new_word, cum_count)
+    # print(encoded)
+    # # print(encoded)
+    # # print(len(encoded))
+    # decoded = decode(encoded, cum_count)
+    # # print(decoded)
+    #
+    # if new_word == decoded:
+    #     print("Success")
+    # else:
+    #     print("Fail")
+
+    word = "ARYTMETYKA"
+    prob_table = calc_prob(word)
+    print("Prob table", prob_table)
+
+    encoded, scale3, added_bits = encode("ARYTMETYKA", prob_table)
+
+
+    decoded = decode(encoded, prob_table, scale3)
+    print("Decoded: ", decoded)
